10aa853324b2589e0c5bd895ddfec8726116651f
Vaibhav <b.vaibhav.0012@gmail.com>
2024-12-19 19:51:50 +0530

    Fixed Style and added etch a sketch functionality, need to add buttons now

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 let parent = document.getElementById("container");
 
-for(let i = 0; i < 16; i++) {
-    let div = document.createElement("div");
-    div.className = "square";
-    parent.appendChild(div);
+//Creates a Square Grid with sides entered
+
+function createGrid(side) {
+    let rows = side;
+    while(rows > 0) {
+        let row = document.createElement("div");
+        row.className = "row";
+        parent.appendChild(row);
+        for(let i = 0; i < side; i++) {
+            let div = document.createElement("div");
+            div.className = "square";
+            div.addEventListener("mouseenter", () => {
+                if(div.style.backgroundColor === "white") {
+                    div.style.backgroundColor = "black";
+                } else {
+                    div.style.backgroundColor = "white";
+                }
+            });
+            row.appendChild(div);
+        }
+        rows--;
+    }
 }
+
+createGrid(64);
